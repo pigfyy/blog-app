fd19346bce539f0890198099d209214c3ddf6b97
pigfyy <111782362+pigfyy@users.noreply.github.com>
2023-03-29 14:53:27 -0700

    font #3

--- app/head.jsx
+++ app/head.jsx
@@ -8,8 +8,10 @@ export default function Head() {
         content="Discover the latest news and insights on our blog app. From tech trends to lifestyle tips, we’ve got you covered. Join our community today!"
       />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
-        href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
     </>
